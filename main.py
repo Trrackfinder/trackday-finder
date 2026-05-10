8c0b5a82aae9fe434c6325652baab51d3e7edba2
Trrackfinder <trackfinder2026@outlook.com>
2026-05-10 14:15:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-import requests
-from bs4 import BeautifulSoup
 
 app = FastAPI()
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-
-def get_events():
-
-    urls = [
-        "https://inter-track.be/en/calendar"
-    ]
-
-    events = []
-
-    for url in urls:
-
-        try:
-            r = requests.get(url, headers=HEADERS, timeout=10)
-
-            soup = BeautifulSoup(r.text, "html.parser")
-
-            text = soup.get_text("\n")
-
-            for line in text.splitlines():
-
-                line_lower = line.lower()
-
-                if "mettet" in line_lower:
-                    events.append({
-                        "circuit": "Mettet",
-                        "organisatie": url
-                    })
-
-                elif "croix" in line_lower:
-                    events.append({
-                        "circuit": "Croix",
-                        "organisatie": url
-                    })
-
-        except Exception as e:
-            print(e)
-
-    return events
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+    <body style="font-family:Arial">
+    <h1 Trackday Finder</H1>
+    <p>Server werkt succesvol </p>
+    </body>
+    </html>
+    """
