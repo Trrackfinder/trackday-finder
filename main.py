84a652b32bdba42ef1a5c7d9eeb0ca5084407367
Trrackfinder <trackfinder2026@outlook.com>
2026-05-10 17:39:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,12 +88,19 @@ def home():
         background: #f5f5f5;
     }
 
-    .card {
-        background: white;
-        padding: 15px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-    }
+    html += f"""
+
+<div class="card">
+
+    <h2>{e['circuit']}</h2>
+
+    <p><b>📅 Datum:</b> {e['date']}</p>
+
+    <p><b>🏢 Organisatie:</b> {e['organisatie']}</p>
+
+</div>
+
+"""
 
     </style>
 
