79ca63cc7fe00f96f3386dbe321b391dd5759c74
Trrackfinder <trackfinder2026@outlook.com>
2026-05-12 15:01:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse, Response
 import requests
@@ -12,7 +13,10 @@ from urllib.parse import urljoin, quote
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 HEADERS = {
     "User-Agent": (
