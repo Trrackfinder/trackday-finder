438b9c49a4be0e95c3330d7b6aa4943224b7fddf
Trrackfinder <trackfinder2026@outlook.com>
2026-05-13 15:53:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1496,7 +1496,7 @@ def home(
 
 <script>
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag(){{dataLayer.push(arguments);}}
 gtag('js', new Date());
 
 gtag('config', 'G-11D49KBTB8');
